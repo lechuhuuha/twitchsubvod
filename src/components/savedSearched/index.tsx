dd44863526f75a5ecd4d06f196fec3a70a4dc801
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2021-06-08 09:21:38 +0700

    delete and select item

--- src/components/savedSearched/index.tsx
+++ src/components/savedSearched/index.tsx
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import { getLocalStorage } from '../../services/localStorage';
+import initLocalStorage, {
+  deleteItem,
+  getLocalStorage,
+} from '../../services/localStorage';
 import { Container, Ul, Li } from './styles';
 
 interface savedSearchedProps {
   onUpdate: Function;
+  onSelect: Function;
 }
 
 const SavedSearched = (props: any) => {
   // props.onUpdate();
   let arrUser = getLocalStorage();
+
   let li = arrUser.map((item, key) => {
     return (
       <Li key={key}>
-        <a href="">{item}</a>
+        <button
+          onClick={() => {
+            props.onSelect(item);
+          }}
+        >
+          {item}
+        </button>
+        <button
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            deleteItem(item);
+            props.onUpdate();
+          }}
+        >
+          X
+        </button>
       </Li>
     );
   });
@@ -20,7 +40,7 @@ const SavedSearched = (props: any) => {
     <>
       <Container>
         <h3>List of streamers you have searched</h3>
-        <Ul>{li}</Ul>
+        <Ul> {li}</Ul>
       </Container>
     </>
   );
