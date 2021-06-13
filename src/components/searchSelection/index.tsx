463d6d0ff8aebc38a3de45b34b1093f753e2339c
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2021-06-13 08:56:27 +0700

    search type

--- src/components/searchSelection/index.tsx
+++ src/components/searchSelection/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Container } from '../QualitySelection/styles';
 import InfoModal from '../InfoModal';
+import { Link, useHistory } from 'react-router-dom';
 
-const SearchSelection = ({ ...rest }): any => {
+const SearchSelection = (props: any): any => {
+  // let history = useHistory();
+  // function handleChange(value: any) {
+  //   history.push(`/search`);
+  // }
+
   return (
     <Container>
-      <label htmlFor="quality">Select a type of search :  </label><br />
-      <select name="quality" id="searchQuality" {...rest}>
+      <label htmlFor="search">Select a type of search : </label>
+      <br />
+      <select
+        onChange={(e) => {
+          props.onChange(e.target.value);
+        }}
+        name="search"
+        id="searchQuality"
+        {...props}
+      >
+        <option value="">Choose a type</option>
         <option value="categories">Categories</option>
         <option value="channels">Channels</option>
       </select>
